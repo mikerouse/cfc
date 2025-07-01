eb4264182715d03784f579aa037ab070fe4dd054
Mike Rouse <mike@mikerouse.uk>
2025-07-01 16:30:06 +0100

    Document view logic with inline comments

--- council_finance/urls.py
+++ council_finance/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('plugins/', include('core.urls')),
+    path('councils/', views.council_list, name='council_list'),
+    path('councils/<slug:slug>/', views.council_detail, name='council_detail'),
 ]
