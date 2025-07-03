955942beaa5adb14928f2b0705d55910df91f1ca
Mike Rouse <mike@mikerouse.uk>
2025-07-03 08:13:51 +0100

    Fix template filter setup

--- council_finance/templatetags/__init__.py
+++ council_finance/templatetags/__init__.py
@@ -1,10 +1,1 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def get_item(obj, key):
-    """Retrieve dictionary value by key in templates."""
-    if isinstance(obj, dict):
-        return obj.get(key)
-    return None
+# Register template tags package
